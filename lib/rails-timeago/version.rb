421b32c4b6ba177d645ff04aa26a01897b60963d
Iris Molinari <molinari@acsinfo.ch>
2014-05-01 10:29:41 +0200

    change versioning notation

--- lib/rails-timeago/version.rb
+++ lib/rails-timeago/version.rb
@@ -3,8 +3,8 @@ module Rails
     module VERSION
       MAJOR = 2
       MINOR = 10
-      PATCH = 99
-      STAGE = nil
+      PATCH = 1
+      STAGE = 1
 
       def self.to_s
         [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join '.'
